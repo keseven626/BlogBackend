c00599c0131c9855816d005e1bf1e811ff8c3599
kesevan626 <87601398+kesevan626@users.noreply.github.com>
2022-11-02 18:50:54 -0700

    Commiting

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,20 @@ from  django.contrib.auth.models import  AbstractBaseUser, PermissionsMixin, Bas
 
 # Create your models here.
 class  useraccountManager(BaseUserManager):
-          def create_user(self, email, name, password=None):
+            def create_user(self, email, name, password=None):
                     if not  email:
                               raise ValueError('Users must have a Email address')
                     email = self.normalize_email(email) 
                     User = self.model(email=email, name=name)
+                    User.set_password(password)
+                    User.save()
+                    return User
+
+            def create_superuser(self, email, name, password=None):
+                    if not  email:
+                              raise ValueError('Users must have a Email address')
+                    email = self.normalize_email(email) 
+                    User = self.model(email=email, name=name, is_superuser=True)
                     User.set_password(password)
                     User.save()
                     return User
@@ -23,7 +32,7 @@ class  useraccount(AbstractBaseUser, PermissionsMixin):
           followers = models.ManyToManyField(settings.AUTH_USER_MODEL)
           is_active = models.BooleanField(default=False)
           is_staff = models.BooleanField(default=False)
-
+          is_superuser = models.BooleanField(default=False)
           objects = useraccountManager()
 
           USERNAME_FIELD = 'email'
